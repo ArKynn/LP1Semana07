9510c35430cfee5fadf0bc95de99bc8339326fff
David Mendes <david.f.mendes@proton.me>
2023-04-27 13:50:18 +0100

    Added several Sphere objects, Threw, popped and checked each sphere status.

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     private static void Main()
     {
+        Sphere red = new Sphere(new Color(255, 0, 0), 5, "red");
+        Sphere green = new Sphere(new Color(0, 255, 0), 5, "green");
+        Sphere blue = new Sphere(new Color(0, 0, 255), 5, "blue");
+
+        Sphere[] allSpheres = new Sphere[] { red, green, blue }; 
         
+        red.Throw();
+        green.Pop();
+        blue.Throw();
+        
+        red.Throw();
+        red.Throw();
+
+        foreach (Sphere sphere in allSpheres)
+        {
+            Console.WriteLine($"{sphere.GetName()} color = ({sphere.color.GetRed()}, {sphere.color.GetGreen()}, {sphere.color.GetBlue()})" +
+                              $" {sphere.GetName()} radius = {sphere.GetRadius()}," +
+                              $" {sphere.GetName()} times thrown = {sphere.GetTimesThrown()}");
+        }
     }
 }
