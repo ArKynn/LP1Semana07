637cb9d7ed83ea7d80eb9f824cf19a6c3cd90ede
David Mendes <david.f.mendes@proton.me>
2023-04-27 13:17:27 +0100

    Added Color class with a constructor, Getter and Setter methods

---
+++ FillSpheres/Color.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Mail;
+
+namespace FillSpheres;
+
+public class Color
+{
+    private int red;
+    private int green;
+    private int blue;
+    private int alpha;
+
+    public Color(int red, int green, int blue, int alpha)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = 255;
+    }
+
+    public int GetRed()
+    {
+        return this.red;
+    }
+
+    public void SetRed(int input)
+    {
+        this.red = input;
+    }
+    
+    public int GetGreen()
+    {
+        return this.green;
+    }
+
+    public void SetGreen(int input)
+    {
+        this.green = input;
+    }
+    
+    public int GetBlue()
+    {
+        return this.blue;
+    }
+
+    public void SetBlue(int input)
+    {
+        this.blue = input;
+    }
+    
+    public int GetAlpha()
+    {
+        return this.alpha;
+    }
+
+    public void SetAlpha(int input)
+    {
+        this.alpha = input;
+    }
+}
