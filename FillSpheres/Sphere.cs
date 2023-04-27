52d0a5046eb86d1ab2e69515df8fab400cbae5db
David Mendes <david.f.mendes@proton.me>
2023-04-27 13:26:22 +0100

    Added Sphere Class with a contructor, Pop, Throw and GetTimesThrown methods

---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace FillSpheres;
+
+public class Sphere
+{
+    private Color color;
+    private int radius;
+    private int timesThrown;
+
+    public Sphere(Color color, int radius)
+    {
+        this.color = color;
+        this.radius = radius;
+        timesThrown = 0;
+    }
+
+    public void Pop()
+    {
+        this.radius = 0;
+    }
+
+    public void Throw()
+    {
+        if (this.radius > 0)
+        {
+            timesThrown++;
+        }
+    }
+
+    public int GetTimesThrown()
+    {
+        return timesThrown;
+    }
+}
