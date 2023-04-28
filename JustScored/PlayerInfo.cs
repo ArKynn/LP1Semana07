036708832a0844861d67b0b280e52c5b77522ffd
David Mendes <david.f.mendes@proton.me>
2023-04-28 14:18:38 +0100

    Added RateofSuccess, MaxScore and Name proprieties, AddGame Method and constructor

---
+++ JustScored/PlayerInfo.cs
@@ -0,0 +1,44 @@
+﻿namespace JustScored;
+
+public class PlayerInfo
+{
+    private float maxScore;
+    private int gamesPlayed;
+    private int gamesWon;
+
+    public float MaxScore
+    {
+        get => maxScore;
+        set
+        {
+            if (value > maxScore) maxScore = value;
+        }
+    }
+    
+    public string Name { get; }
+
+    public float RateOfSuccess
+    {
+        get
+        {
+            float rateOfSuccess = 0;
+            if (gamesPlayed > 0) rateOfSuccess = gamesWon / gamesPlayed;
+
+            return rateOfSuccess;
+        }
+    }
+
+    public void AddGame(bool win)
+    {
+        gamesPlayed++;
+        if (win == true) gamesWon++;
+    }
+
+    public PlayerInfo(string Name)
+    {
+        gamesWon = 0;
+        gamesPlayed = 0;
+        this.Name = Name;
+        maxScore = 0;
+    }
+}    
