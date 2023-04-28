8af2d9233e7b1fd641b55f04a5329b02a0760257
David Mendes <david.f.mendes@proton.me>
2023-04-28 14:25:20 +0100

    Added FillSpheres Color class with proprieties

---
+++ BetterFillSpheres/Color.cs
@@ -0,0 +1,28 @@
+﻿namespace BetterFillSpheres;
+
+
+public class Color
+{
+    private int red { get; set; }
+    private int green { get; set; }
+    private int blue { get; set; }
+    private int alpha { get; set; }
+
+    public Color(int red, int green, int blue)
+    {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        alpha = 255;
+    }
+
+    
+    public int GetGrey
+    {
+        get
+        {
+            return (this.red + this.green + this.blue) / 3;    
+        }
+        
+    }
+}    
